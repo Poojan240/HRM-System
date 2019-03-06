589e4bfb9a3d408e5d0a37354318e40632c9549f
Poojan240 <36248614+Poojan240@users.noreply.github.com>
2019-03-06 09:39:46 -0600

    Update populate.sql

--- ProjectHRM/src/main/sql/populate.sql
+++ ProjectHRM/src/main/sql/populate.sql
@@ -20,9 +20,9 @@ INSERT INTO projecthrm.address VALUES (NULL,"Fairfield","fsdfsd@gmail.com","Iowa
 INSERT INTO projecthrm.address VALUES (NULL,"Fairfield","fsdfsd@gmail.com","Iowa","100Nth",52557);
 INSERT INTO projecthrm.address VALUES (NULL,"Fairfield","fsdfsd@gmail.com","Iowa","100Nth",52557);
 
-INSERT INTO projecthrm.employee VALUES (NULL,"2015-12-16","2015-12-16","2015-12-16","Nabin","Male",1,"2015-12-16","Puri","2015-12-16",1234,1,1,1,1,1,1,1,1);
-INSERT INTO projecthrm.employee VALUES (NULL,"2015-12-16","2015-12-16","2015-12-16","Ashok","Male",2,"2015-12-16","Balami","2015-12-16",1234,2,1,1,1,1,1,1,2);
-INSERT INTO projecthrm.employee VALUES (NULL,"2015-12-16","2015-12-16","2015-12-16","Nidina","Male",3,"2015-12-16","Shrestha","2015-12-16",1234,3,1,1,1,1,1,1,3);
+INSERT INTO projecthrm.employee VALUES (NULL,"2015-12-16","2015-12-16","2015-12-16","Poojan","Male",1,"2015-12-16","Pradhan","2015-12-16",1234,1,1,1,1,1,1,1,1);
+INSERT INTO projecthrm.employee VALUES (NULL,"2015-12-16","2015-12-16","2015-12-16","Jack","Male",2,"2015-12-16","Smith","2015-12-16",1234,2,1,1,1,1,1,1,2);
+INSERT INTO projecthrm.employee VALUES (NULL,"2015-12-16","2015-12-16","2015-12-16","Monica","Female",3,"2015-12-16","Keller","2015-12-16",1234,3,1,1,1,1,1,1,3);
 
 
 INSERT INTO projecthrm.leaves VALUES (1,"2015-12-16","2015-12-16","2015-12-16","dsfsdldffdkf",1,1);

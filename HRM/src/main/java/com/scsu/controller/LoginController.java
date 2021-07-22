6c79a73e733c2672333f1b270b3ceb7bb4a1a500
Poojan Pradhan <poojanbahadurpradhan@gmail.com>
2021-07-21 20:36:35 -0400

    Test file.

--- HRM/src/main/java/com/scsu/controller/LoginController.java
+++ HRM/src/main/java/com/scsu/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 //Test Message
+//Test Message2
 
 @Controller
 public class LoginController {
